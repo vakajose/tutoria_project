d7bcea5c57692b1b77bc58c06d7074aba2b07e98
Jose Luis Vaca Fernandez <vakajose@gmail.com>
2024-06-17 21:20:08 -0400

    cors policy added

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.api import api_blueprint
 import os
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/api/*": {"origins": "*"	}})
 # Registro de Blueprint para las rutas de la API
 app.register_blueprint(api_blueprint, url_prefix='/api')
 
